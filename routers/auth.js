3409ca11584bb00eba074c802c7aad73b7ed63af
Taner <taneozer16@gmail.com>
2023-06-15 19:39:53 +0300

    register login logout imageUpload added

---
+++ routers/auth.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const {
+  register,
+  login,
+  logout,
+  imageUpload,
+} = require("../controllers/auth.js");
+const photoUpload = require("../helpers/libraries/multer.js");
+const { getAccessToRoute }= require("../middlewares/authorization/auth.js");
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/logout", getAccessToRoute, logout);
+
+router.put(
+  "/upload",
+  [getAccessToRoute, photoUpload.single("profile_image")],
+  imageUpload
+);
+
+module.exports = router;
