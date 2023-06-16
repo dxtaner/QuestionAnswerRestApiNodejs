7b696c9a67b0427486cb78db230f57a6da829596
Taner <taneozer16@gmail.com>
2023-06-16 14:59:56 +0300

    getLoggedInUser,   resetPassword,   updateDetails, added

--- routers/auth.js
+++ routers/auth.js
@@ -6,6 +6,9 @@ const {
   logout,
   imageUpload,
   forgotPassword,
+  getLoggedInUser,
+  resetPassword,
+  updateDetails,
 } = require("../controllers/auth.js");
 const photoUpload = require("../helpers/libraries/multer.js");
 const { getAccessToRoute }= require("../middlewares/authorization/auth.js");
@@ -14,12 +17,15 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", getAccessToRoute, logout);
 
+router.get("/user",getAccessToRoute,getLoggedInUser);
 router.put(
   "/upload",
   [getAccessToRoute, photoUpload.single("profile_image")],
   imageUpload
 );
 
-router.post("/forgotpassword",forgotPassword)
+router.put("/edit",getAccessToRoute,updateDetails);
+router.post("/forgotpassword",forgotPassword);
+router.put("/resetpassword",resetPassword);
 
 module.exports = router;
