0386bc0644a99b468d8d9d7b83156ea93e9ccab0
Taner <taneozer16@gmail.com>
2023-06-17 01:40:26 +0300

    checkQuestionExist added

--- middlewares/database/databaseErrorHelpers.js
+++ middlewares/database/databaseErrorHelpers.js
@@ -2,6 +2,7 @@ const path = require("path");
 const root = path.dirname(require.main.filename);
 
 const User = require(root + "/models/User.js");
+const Question = require(root + "/models/Question.js");
 
 const errorWrapper = require(root + "/helpers/error/errorWrapper.js");
 const CustomError = require(root + "/helpers/error/customError.js");
@@ -17,6 +18,20 @@ const checkUserExist = errorWrapper(async (req, res, next) => {
   next();
 });
 
+const checkQuestionExist = errorWrapper(async (req, res, next) => {
+  const question_id = req.params.id || req.params.question_id;
+
+  const question = await Question.findById(question_id);
+
+  if (!question) {
+    return next(
+      new CustomError(`Question Not Found with Id : ${question_id}`, 404)
+    );
+  }
+  next();
+});
+
 module.exports = {
   checkUserExist,
+  checkQuestionExist,
 };
