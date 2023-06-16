48ba87a987925a8819cbd534b8a2fd3d650c8a90
Taner <taneozer16@gmail.com>
2023-06-17 01:40:06 +0300

    getAllQuestions,   getSingleQuestion,   editQuestion, added

--- controllers/question.js
+++ controllers/question.js
@@ -17,6 +17,44 @@ const askNewQuestion = errorWrapper(async (req, res, next) => {
   });
 });
 
+const getAllQuestions = errorWrapper(async (req, res, next) => {
+  const questions = await Question.find();
+
+  return res.status(200).json({
+    success: true,
+    data: questions,
+  });
+});
+
+const getSingleQuestion = errorWrapper(async (req, res, next) => {
+  const { id } = req.params;
+  const question = await Question.findById(id);
+
+  return res.status(200).json({
+    success: true,
+    data: question,
+  });
+});
+
+const editQuestion = errorWrapper(async (req, res, next) => {
+  const { id } = req.params;
+  const { title, content } = req.body;
+  let question = await Question.findById(id);
+
+  question.title = title;
+  question.content = content;
+
+  question = await question.save();
+
+  res.status(200).json({
+    success: true,
+    data: question,
+  });
+});
+
 module.exports = {
   askNewQuestion,
+  getAllQuestions,
+  getSingleQuestion,
+  editQuestion,
 };
