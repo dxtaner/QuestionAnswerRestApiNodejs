7ad246a01b6cdd650024297dced40c52a213387a
Taner <taneozer16@gmail.com>
2023-06-16 14:58:09 +0300

    adminrouter added

---
+++ routers/admin.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const User = require("../models/User.js");
+
+const {
+    getAccessToRoute,
+    getAdminAccess
+} = require("../middlewares/authorization/auth.js");
+const {
+    deleteUser,
+    getBlockUser
+}  = require("../controllers/admin.js");
+
+const {
+    checkUserExist
+} = require("../middlewares/database/databaseErrorHelpers.js");
+
+const router = express.Router();
+
+// users, user, delete, block
+
+router.use([getAccessToRoute,getAdminAccess]);
+
+
+router.get("/block/:id",checkUserExist,getBlockUser);
+router.delete("/user/:id",checkUserExist,deleteUser);
+
+
+module.exports = router;
