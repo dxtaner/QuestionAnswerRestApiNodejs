0ff7a4f58988d4650ccb52d596928b6b61623830
Taner <taneozer16@gmail.com>
2023-06-17 21:22:43 +0300

    edited admin routes

--- routers/admin.js
+++ routers/admin.js
@@ -1,28 +1,22 @@
 const express = require("express");
-const User = require("../models/User.js");
 
 const {
-    getAccessToRoute,
-    getAdminAccess
+  getAccessToRoute,
+  getAdminAccess,
 } = require("../middlewares/authorization/auth.js");
-const {
-    deleteUser,
-    getBlockUser
-}  = require("../controllers/admin.js");
+const { deleteUser, getBlockUser } = require("../controllers/admin.js");
 
 const {
-    checkUserExist
+  checkUserExist,
 } = require("../middlewares/database/databaseErrorHelpers.js");
 
 const router = express.Router();
 
 // users, user, delete, block
 
-router.use([getAccessToRoute,getAdminAccess]);
+router.use([getAccessToRoute, getAdminAccess]);
 
-
-router.get("/block/:id",checkUserExist,getBlockUser);
-router.delete("/user/:id",checkUserExist,deleteUser);
-
+router.get("/block/:id", checkUserExist, getBlockUser);
+router.delete("/user/:id", checkUserExist, deleteUser);
 
 module.exports = router;
