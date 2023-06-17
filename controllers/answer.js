def7295901dfe20bedd96fac834ee8a2c87a8caa
Taner <taneozer16@gmail.com>
2023-06-17 22:47:57 +0300

    getSingleAnswer method added

--- controllers/answer.js
+++ controllers/answer.js
@@ -36,7 +36,21 @@ const getAllAnswersByQuestion = errorWrapper(async (req, res, next) => {
   });
 });
 
+const getSingleAnswer = errorWrapper(async (req, res, next) => {
+  const { answer_id } = req.params;
+
+  const answer = await Answer.findById(answer_id)
+    .populate({ path: "user", select: "name profile_image" })
+    .populate({ path: "question", select: "title" });
+
+  res.status(200).json({
+    success: true,
+    data: answer,
+  });
+});
+
 module.exports = {
   addNewAnswerToQuestion,
   getAllAnswersByQuestion,
+  getSingleAnswer,
 };
