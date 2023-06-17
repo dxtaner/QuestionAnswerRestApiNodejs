40b8c98c456d89cac605bb9fa564d8c3ce5f6977
Taner <taneozer16@gmail.com>
2023-06-17 22:48:25 +0300

    answer_id  router added

--- routers/answer.js
+++ routers/answer.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const { getAccessToRoute } = require("../middlewares/authorization/auth.js");
-const { addNewAnswerToQuestion,getAllAnswersByQuestion } = require("../controllers/answer.js");
+const {
+  addNewAnswerToQuestion,
+  getAllAnswersByQuestion,
+  getSingleAnswer,
+} = require("../controllers/answer.js");
+const {
+  checkQuestionAndAnswerExist,
+} = require("../middlewares/database/databaseErrorHelpers");
 
 router.post("/", [getAccessToRoute], addNewAnswerToQuestion);
 
 router.get("/", getAllAnswersByQuestion);
+router.get("/:answer_id", checkQuestionAndAnswerExist, getSingleAnswer);
 
 module.exports = router;
