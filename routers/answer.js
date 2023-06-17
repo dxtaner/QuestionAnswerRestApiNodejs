460bfbb838b36e814baf7d6a1665a41be742057a
Taner <taneozer16@gmail.com>
2023-06-17 22:39:25 +0300

    getAllAnswersByQuestion router added

--- routers/answer.js
+++ routers/answer.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const { getAccessToRoute } = require("../middlewares/authorization/auth.js");
-const { addNewAnswerToQuestion } = require("../controllers/answer.js");
+const { addNewAnswerToQuestion,getAllAnswersByQuestion } = require("../controllers/answer.js");
 
-router.post(
-  "/",
-  [getAccessToRoute],
-  addNewAnswerToQuestion
-);
+router.post("/", [getAccessToRoute], addNewAnswerToQuestion);
+
+router.get("/", getAllAnswersByQuestion);
 
 module.exports = router;
