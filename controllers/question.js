5c94b61e2674002d7eedd537c0d4d20a77ac57c4
Taner <taneozer16@gmail.com>
2023-06-17 16:13:51 +0300

    likeQuestion,   undoLikeQuestion, addded

--- controllers/question.js
+++ controllers/question.js
@@ -63,10 +63,54 @@ const deleteQuestion = errorWrapper(async (req, res, next) => {
   });
 });
 
+const likeQuestion = errorWrapper(async (req, res, next) => {
+  const { id } = req.params;
+
+  const question = await Question.findById(id);
+
+  if (question.likes.includes(req.user.id)) {
+    return next(new CustomError("You already liked this question", 400));
+  }
+  question.likes.push(req.user.id);
+  question.likeCount += 1;
+
+  await question.save();
+
+  return res.status(200).json({
+    success: true,
+    data: question,
+  });
+});
+
+const undoLikeQuestion = errorWrapper(async (req, res, next) => {
+  const { id } = req.params;
+
+  const question = await Question.findById(id);
+
+  if (!question.likes.includes(req.user.id)) {
+    return next(
+      new CustomError("You can not undo like operation for this question", 400)
+    );
+  }
+  const index = question.likes.indexOf(req.user.id);
+
+  question.likes.splice(index, 1);
+  question.likeCount -= 1;
+
+  await question.save();
+
+  res.status(200).json({
+    success: false,
+    data: question,
+  });
+});
+
 module.exports = {
   askNewQuestion,
   getAllQuestions,
   getSingleQuestion,
   editQuestion,
   deleteQuestion,
+  likeQuestion,
+  undoLikeQuestion,
 };
