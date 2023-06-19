2dfdfd1ae003a133cbdc25f070bdf15bce72ef00
Taner <taneozer16@gmail.com>
2023-06-19 16:47:43 +0300

    limitAccess add

--- server.js
+++ server.js
@@ -4,18 +4,26 @@ const routes = require("./routers");
 const connectDatabase = require("./helpers/database/connectDatabase.js");
 
 const errorHandler = require("./middlewares/errors/errorHandler.js");
-
+const limitAccess = require("./middlewares/security/limitAccess.js");
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 // Environment Variables
 
-dotenv.config({path : "./config/config.env"});
+dotenv.config({ path: "./config/config.env" });
 
+// Security
+
+app.use(
+  limitAccess({
+    windowMs: 10 * 60 * 1000, // 10 Minutes
+    max: 500,
+  })
+);
 
 // Routes
-app.use("/api",routes);
+app.use("/api", routes);
 
 // MongoDb Connection
 connectDatabase();
@@ -27,6 +35,8 @@ app.use(express.static("public"));
 app.use(errorHandler);
 
 // Starting Our Server
-app.listen(PORT,() => {
-    console.log(`App Started on ${PORT} - Environment : ${process.env.NODE_ENV} `);
+app.listen(PORT, () => {
+  console.log(
+    `App Started on ${PORT} - Environment : ${process.env.NODE_ENV} `
+  );
 });
