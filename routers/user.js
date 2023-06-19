49b87b84364c70953e2b846f498be1cf59e0fb42
Taner <taneozer16@gmail.com>
2023-06-19 16:55:55 +0300

    userQueryMiddleware added

--- routers/user.js
+++ routers/user.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const User = require("../models/User");
+const User = require("../models/User.js");
 const {
     checkUserExist
 } = require("../middlewares/database/databaseErrorHelpers.js");
 
 // users,profile
 const { getSingleUser, getAllUsers} = require("../controllers/admin.js");
+const userQueryMiddleware = require("../middlewares/query/userQueryMiddleware.js");
 
 const router = express.Router();
 
 // Get All Users
-router.get("/",getAllUsers);
+router.get("/",userQueryMiddleware(User),getAllUsers);
 
 // Get Single User Profile
 router.get("/profile/:id", checkUserExist,getSingleUser);
