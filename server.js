1c3a9bfc654ff41ace78aafa951c61460f6502f1
Taner <taneozer16@gmail.com>
2023-06-20 17:55:18 +0300

    edited security node

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const routes = require("./routers");
 const connectDatabase = require("./helpers/database/connectDatabase.js");
 
+const dotenv = require("dotenv");
+const routes = require("./routers");
 const errorHandler = require("./middlewares/errors/errorHandler.js");
+
 const limitAccess = require("./middlewares/security/limitAccess.js");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const helmet = require("helmet");
+const cors = require("cors");
+
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 // Environment Variables
-
 dotenv.config({ path: "./config/config.env" });
 
 // Security
-
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
 app.use(
   limitAccess({
-    windowMs: 10 * 60 * 1000, // 10 Minutes
+    windowMs: 10 * 60 * 500, // 5 Minutes
     max: 500,
   })
 );
+app.use(hpp());
+app.use(cors());
 
 // Routes
 app.use("/api", routes);
